4e8c19eb9722ca9138ac53e62e05527d71fb0960
Vibha <vibha.gopal@gmail.com>
2019-05-06 15:04:02 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url        'https://github.com/sous-chefs/mongodb' if respond_to?(:source_url)
 issues_url        'https://github.com/sous-chefs/mongodb/issues' if respond_to?(:issues_url)
 chef_version      '>= 13.0'
-version           '2.0.0'
+version           '2.0.1'
 
 depends 'build-essential', '>= 5.0.0'
 
